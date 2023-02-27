2c1a05b773bd41a82d7ea9597a72d87b887ba8bf
proj.mgr.tw@gmail.com <proj.mgr.tw@gmail.com>
2023-02-27 05:41:12 +0000

    model for stage

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,18 @@
+select
+    o.orderid,
+    o.orderdate,
+    o.shipdate,
+    o.shipmode,
+    o.ordersellingprice - o.ordercostprice as orderprofit,
+    o.ordersellingprice,
+    o.ordercostprice,
+    c.customername,
+    c.segment,
+    c.state,
+    p.category,
+    p.productname,
+    p.subcategory
+
+from {{ ref("raw_orders") }} as o
+left join {{ ref("raw_customers") }} as c on o.customerid = c.customerid
+left join {{ ref("raw_product") }} as p on o.productid = p.productid
